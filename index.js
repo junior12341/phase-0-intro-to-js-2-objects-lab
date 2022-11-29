29c854120866dd240d8335053cb41412bab730c0
Hubert Jr <hubertjunior254@icloud.com>
2022-11-29 12:52:55 +0300

    work done

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution in this file!
+const employee = {
+    name: "Hubert Jr",
+    streetAddress: "LA"
+}
+
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+       ... employee,
+       [key] : value 
+    }
+}
+
+console.log (updateEmployeeWithKeyAndValue)(employee,"Hubert")
+console.log(employee);
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value 
+    return employee
+}
+
+destructivelyUpdateEmployeeWithKeyAndValue(employee, "Hubert", value)
+console.log(employee)
+
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {
+        ...obj 
+    }
+    delete newEmployee[key]
+    return newEmployee
+
+}
+console.log(deleteFromEmployeeByKey(employee ,"JobIdentificationNumber" ))
+console.log(employee);
+
+destructivelyDeleteFromEmployeeByKey(employee, key) 
